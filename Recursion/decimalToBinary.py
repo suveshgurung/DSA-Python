7a3eeba05575544890e726564be806f1abad538b
suvesh <suveshgurung2@gmail.com>
2024-10-25 14:28:36 +0545

    Recursion: decimal number to binary

---
+++ Recursion/decimalToBinary.py
@@ -0,0 +1,9 @@
+def decimalToBinary(n):
+    assert (int(n) == n), "The number should be an integer"
+    quotient = int(n / 2)
+    if (quotient == 0):
+        return (n % 2)
+    else:
+        return ((n % 2) + (10 * decimalToBinary(quotient)))
+
+print(decimalToBinary(-13))
